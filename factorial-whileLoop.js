3b30c07c5a7775a38cec8fa9961a5f089535244d
noman-swe <noman.swe130@gmail.com>
2022-02-02 16:55:20 +0600

    Factorial with while loop

---
+++ factorial-whileLoop.js
@@ -0,0 +1,17 @@
+function getFactorial(value){
+     
+    let factorial = 1;
+    let i = 1;
+   while( i <= value){
+        factorial = factorial * i;
+        i++;
+    }
+    return factorial;
+   //  console.log(fact);
+}
+
+var firstFactorial = getFactorial(7);
+console.log('factorial of 7 is: ' + firstFactorial);
+
+var secondFactorial = getFactorial(9);
+console.log('factorial of 9 is :' + secondFactorial);
