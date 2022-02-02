1839e3d43f3ace3b6a65c6b59ebf681d12dd7e2f
noman-swe <noman.swe130@gmail.com>
2022-02-02 17:04:52 +0600

    M20-7:decrementing loop Factorial

--- factorial-whileLoop.js
+++ factorial-whileLoop.js
@@ -1,3 +1,4 @@
+/*
 function getFactorial(value){
      
     let factorial = 1;
@@ -10,6 +11,23 @@ function getFactorial(value){
    //  console.log(fact);
 }
 
+var firstFactorial = getFactorial(7);
+console.log('factorial of 7 is: ' + firstFactorial);
+
+var secondFactorial = getFactorial(9);
+console.log('factorial of 9 is :' + secondFactorial);
+*/
+
+function getFactorial(number){
+    let factorial = 1;
+    let i = number;
+
+    while(i >= 1){
+        factorial = factorial * i;
+        i--;
+    }
+    return factorial;
+}
 var firstFactorial = getFactorial(7);
 console.log('factorial of 7 is: ' + firstFactorial);
 
